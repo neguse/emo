2948301d250beb8f46d29d7495ae5a6991245f8c
neguse <negcee@gmail.com>
2025-01-25 23:54:16 +0900

    add emo

---
+++ src/app/domain/lang.ts
@@ -0,0 +1,163 @@
+export const MemoryRows = 16;
+export const MemoryCols = 8;
+export const MemorySize = MemoryRows * MemoryCols;
+const InitPointer = MemoryCols * 8;
+export const ProgramSize = MemorySize - InitPointer;
+
+export class MemoryByte {
+    private value: number;
+    private emo: string;
+
+    constructor() {
+        this.value = 0;
+        this.emo = '';
+    }
+
+    getVal(): number {
+        return this.value;
+    }
+    getEmo(): string {
+        return this.emo;
+    }
+    setVal(val: number): void {
+        this.value = val;
+    }
+    setEmo(emo: string): void {
+        this.emo = emo;
+    }
+
+    assign(b: MemoryByte) {
+        this.value = b.getVal();
+        this.emo = b.getEmo();
+    }
+
+    isMovable(): boolean {
+        return this.emo === '😭';
+    }
+
+    isBlock(): boolean {
+        return this.emo === '🧱' || this.emo === '😭';
+    }
+}
+
+class Memory {
+    private memory: MemoryByte[];
+
+    constructor() {
+        this.memory = Array(MemorySize).fill(0).map(() => new MemoryByte());
+    }
+
+    get(index: number): MemoryByte {
+        if (index < 0 || index >= MemorySize) {
+            throw new Error('Memory index out of bounds');
+        }
+        return this.memory[index];
+    }
+
+    at(row: number, col: number): MemoryByte {
+        return this.get(row * MemoryCols + col);
+    }
+
+    length(): number {
+        return this.memory.length;
+    }
+
+    copy(): MemoryByte[] {
+        return this.memory.map(b => {
+            const copy = new MemoryByte();
+            copy.assign(b);
+            return copy;
+        });
+    }
+}
+
+
+export class Language {
+    private memory: Memory;
+    private pointer: number;
+
+    constructor() {
+        this.memory = new Memory();
+        this.pointer = InitPointer;
+    }
+
+    init(programText: string[], memory: MemoryByte[]) {
+        if (programText.length > ProgramSize) {
+            throw new Error('Program size exceeded');
+        }
+        if (memory.length > MemorySize) {
+            throw new Error('Memory size exceeded');
+        }
+        memory.forEach((b, index) => {
+            const m = this.memory.get(index);
+            m.setEmo(b.getEmo());
+            m.setVal(b.getVal());
+        });
+        for (let i = InitPointer; i < MemorySize; i++) {
+            this.memory.get(i).setEmo('');
+            this.memory.get(i).setVal(0);
+        }
+        programText.forEach((op, index) => {
+            this.memory.get(index + InitPointer).setEmo(op);
+        });
+        this.pointer = InitPointer;
+    }
+
+    step() {
+        if (this.pointer >= this.memory.length()) {
+            return;
+        }
+        const op = this.memory.get(this.pointer).getEmo();
+        this.pointer++;
+        switch (op) {
+            case '👈️':
+                this.opMove(-1);
+                break;
+            case '👇️':
+                this.opMove(MemoryCols);
+                break;
+            case '👆️':
+                this.opMove(-MemoryCols);
+                break;
+            case '👉️':
+                this.opMove(1);
+                break;
+        }
+    }
+
+    opMove(delta: number) {
+        if (delta > 0) {
+            for (let i = MemorySize - 1; i >= 0; i--) {
+                const b = this.memory.get(i);
+                if (b.isMovable()) {
+                    const next = this.memory.get(i + delta);
+                    if (next.isBlock()) {
+                        continue;
+                    }
+                    next.setEmo(b.getEmo());
+                    b.setEmo('');
+                }
+            }
+        } else {
+            for (let i = 0; i < MemorySize; i++) {
+                const b = this.memory.get(i);
+                if (b.isMovable()) {
+                    const next = this.memory.get(i + delta);
+                    if (next.isBlock()) {
+                        continue;
+                    }
+                    next.setEmo(b.getEmo());
+                    b.setEmo('');
+                }
+            }
+        }
+    }
+
+    copyMemory(): MemoryByte[] {
+        return this.memory.copy();
+    }
+
+    getPointer(): number {
+        return this.pointer;
+    }
+}
---
+++ src/app/editor.tsx
@@ -0,0 +1,157 @@
+'use client';
+
+import { useState, useRef, useEffect } from "react";
+import { MemoryRows, MemoryCols, Language, MemoryByte } from "./domain/lang";
+
+
+const keys : string[] = [
+    '👈️',
+    '👇️',
+    '👆️',
+    '👉️',
+    '←',
+];
+
+function Memory({memory, pointer}: {memory: MemoryByte[], pointer: number}) {
+    const rows = [];
+
+    // header
+    const header = [];
+    header.push(
+        <th key={-1}>
+            {' '}
+        </th>
+    );
+    for (let col = 0; col < MemoryCols; col++) {
+        header.push(
+            <th key={col}>
+                {col}
+            </th>
+        );
+    }
+
+    for (let row = 0; row < MemoryRows; row++) {
+        const cols = [];
+        cols.push(
+            <td key={-1} className="cell">
+                {row}
+            </td>
+        );
+        for (let col = 0; col < MemoryCols; col++) {
+            const index = row * MemoryCols + col;
+            if (index >= memory.length) {
+                break;
+            }
+            const isPointer = index === pointer;
+            cols.push(
+                <td key={col} className="cell">
+                    {memory[index].getEmo()}
+                </td>
+            );
+        }
+        rows.push(
+            <tr key={row}>
+                {cols}
+            </tr>);
+    }
+    return (
+        <table className="memory">
+            <thead>
+                <tr>
+                    {header}
+                </tr>
+            </thead>
+            <tbody>
+                {rows}
+            </tbody>
+        </table>
+    );
+}
+
+function Program({text}: {text: string[]}) {
+    return (
+        <textarea className="program" value={text.join('')} readOnly>
+            
+        </textarea>
+    );
+}
+
+function Keyboard({onKey}: { onKey: (key: string) => void }) {
+    return (
+            <div className='keyboard'>
+                {keys.map((key) => (
+                    <button className='key' key={key} onClick={() => onKey(key)}>
+                        {key}
+                    </button>
+                ))}
+            </div>
+    );
+};
+
+function Debug({onReset, onStep}: {onReset: () => void, onStep: () => void}) {
+    return (
+        <div className='debug'>
+            <button className='reset' onClick={onReset}>
+            ↩
+            </button>
+            <button className='step' onClick={onStep}>
+            ➡️
+            </button>
+        </div>
+    );
+}
+
+function initMemory() : MemoryByte[] {
+    const emos = [
+        '🧱', '🧱', '🧱', '🧱', '🧱', '🧱', '🧱', '🧱',
+        '🧱', '', '', '', '', '😭', '', '🧱',
+        '🧱', '', '', '', '', '', '', '🧱',
+        '🧱', '', '', '', '', '', '', '🧱',
+        '🧱', '', '', '', '', '', '', '🧱',
+        '🧱', '', '', '', '', '😭', '', '🧱',
+        '🧱', '', '', '', '', '', '', '🧱',
+        '🧱', '🧱', '🧱', '🧱', '🧱', '🧱', '🧱', '🧱',
+    ];
+    return emos.map((emo) => {
+        const b = new MemoryByte();
+        b.setEmo(emo);
+        return b;
+    });
+}
+
+export default function Editor() {
+    const [memory, setMemory] = useState<MemoryByte[]>([]);
+    const [pointer, setPointer] = useState(0);
+    const [text, setText] = useState<string[]>([]);
+    const lang = useRef(new Language());
+
+    function onKey(key: string) {
+        if (key === '←') {
+            setText(text.slice(0, -1));
+            return;
+        }
+        setText([...text, key]);
+    }
+    function onReset() {
+        lang.current.init(text, initMemory());
+        setMemory(lang.current.copyMemory());
+        setPointer(lang.current.getPointer());
+    }
+    function onStep() {
+        lang.current.step();
+        setMemory(lang.current.copyMemory());
+        setPointer(lang.current.getPointer());
+    }
+    useEffect(() => {
+        onReset();
+    }, []);
+    
+    return (
+        <div>
+            <Memory memory={memory} pointer={pointer} />
+            <Program text={text} />
+            <Keyboard onKey={onKey} />
+            <Debug onReset={onReset} onStep={onStep} />
+        </div>
+    );
+}
